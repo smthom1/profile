5f4dbc4dd07b95c37aaa0fa9798a416cecb13107
Sophia Thompson <119273210+smthom1@users.noreply.github.com>
2026-06-01 15:11:15 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/profile/',
+  // Dynamically set base path when deploying to GitHub Pages to prevent broken assets (blank screen),
+  // while keeping the standard root base path for local development!
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/'
 })
